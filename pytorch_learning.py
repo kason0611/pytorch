6a2fdf6b71d2db49d41eb508066a3b2565d3cbf3
kason0611 <kason0611@gmail.com>
2021-09-23 16:37:09 +0800

    My commit

--- pytorch_learning.py
+++ pytorch_learning.py
@@ -5,3 +5,21 @@ Created on Thu Sep 23 16:26:52 2021
 @author: mikexia
 """
 
+import torchvision
+
+#准备数据集
+
+train_data = torchvision.datasets.CIFAR10(root = '../Pytorch 啃啃啃', train = True, 
+                                          transform = torchvision.transforms.ToTensor(), download = True)
+
+test_data = torchvision.datasets.CIFAR10(root = '../Pytorch 啃啃啃', train = False, 
+                                         transform = torchvision.transforms.ToTensor(), download = True)
+
+
+# length 长度
+train_data_size = len(train_data)
+test_data_size = len(test_data)
+print("训练集长度:{}".format(train_data_size))
+print("测试集长度:{}".format(test_data_size))
+
+# 利用DataLoader 加载数据集
